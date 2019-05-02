43f3c60be3bae6ed76ee93d6a44504cce4efb289
Vladimir Dulenov <originalblack@mail.ru>
2019-05-03 02:07:43 +0300

    add simple server

---
+++ base/server/server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func main() {
+	http.HandleFunc("/", echoString)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
